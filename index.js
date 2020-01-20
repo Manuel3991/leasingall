ab660a77418435ad94dc570fcaff97ecf6d77bf4
Manuel3991 <54853086+Manuel3991@users.noreply.github.com>
2020-01-20 12:30:26 -0600

    init commit

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+
+// create new express app and save it as "app"
+const app = express();
+
+// server configuration
+app.set('port', process.env.PORT || 3000);
+
+app.use(express.static('./'));
+
+app.get('/', function(req, res) {
+    res.render('index.html');
+});
+
+
+// // create a route for the app
+// app.get('/', (req, res) => {
+//   //res.send('Hello World');
+//   res.render('index.html')
+// });
+
+// make the server listen to requests
+
+app.listen(app.get('port'),() =>{
+  console.log('server on port',app.get('port'));
+});
